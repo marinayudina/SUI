08bf8f239b19835c691c7483b6bf13e5f4f89cc9
Marina Yudina <0518204@mail.ru>
2023-10-28 19:26:02 +0300

    start

---
+++ Weather/Weather/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Марина on 28.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            Text("MArina")
+            VStack {
+                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                Text("Hello, world!")
+            }
+        }
+        Color = .red
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Марина on 28.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
