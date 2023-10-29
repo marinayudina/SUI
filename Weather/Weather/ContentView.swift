c2bb5842ea79e8a9e2c6b65ccfabd0c4b5154936
Marina Yudina <0518204@mail.ru>
2023-10-29 21:15:29 +0300

    weather app finish

--- Weather/Weather/ContentView.swift
+++ Weather/Weather/ContentView.swift
@@ -8,17 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
     var body: some View {
-        HStack {
-            Text("MArina")
+        ZStack {
+            BackgroundView(isNight: isNight)
+            
             VStack {
-                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                CityTextView(cityName: "Moscow")
+                
+                MainWeatherView(imageName: isNight ? "moon.stars.fill" :  "cloud.sun.fill", temperature: 30)
+  
+                
+                HStack(spacing: 20) {
+                    WeatherDayView(dayofWeek: "TUE", imageName: "sun.max.fill", temperature: 32)
+                    WeatherDayView(dayofWeek: "WED", imageName: "cloud.sun.bolt.fill", temperature: 30)
+                    WeatherDayView(dayofWeek: "THU", imageName: "cloud.fill", temperature: 27)
+                    WeatherDayView(dayofWeek: "FRI", imageName: "wind", temperature: 23)
+                    WeatherDayView(dayofWeek: "SAT", imageName: "sun.max.fill", temperature: 33)
                 }
-                Text("Hello, world!")
+                Spacer()
+                
+                Button {
+                    isNight.toggle()
+                } label: {
+                    WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
+                }
+                Spacer()
             }
         }
-        Color = .red
     }
 }
 
@@ -27,3 +46,69 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct WeatherDayView: View {
+    
+    var dayofWeek: String
+    var imageName: String
+    var temperature: Int
+
+    var body: some View {
+        VStack(spacing: 5){
+            Text(dayofWeek)
+                .font(.system(size: 25, weight: .medium))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .symbolRenderingMode(.multicolor)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+            Text("\(temperature)°")
+                .font(.system(size: 27, weight: .medium))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+    var isNight: Bool
+    
+    var body: some View {
+        ContainerRelativeShape()
+            .fill(isNight ? Color.black.gradient : Color.blue.gradient)
+            .ignoresSafeArea()
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 35, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding() // отступ со всех сторон границ}
+    }
+}
+
+struct MainWeatherView: View {
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack() {
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+            Text("\(temperature)°")
+                .font(.system(size: 72, weight: .medium))
+                .foregroundColor(.white)
+                
+        }
+        .padding(.bottom, 30)
+    }
+}
+
---
+++ Weather/Weather/WeatherButton.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherButton.swift
+//  Weather
+//
+//  Created by Марина on 29.10.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 200, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold))
+            .cornerRadius(10)
+    }
+}
